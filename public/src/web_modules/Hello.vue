a122b86d419e3bacbc250fdefc2ad1c494501b7d
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-03-07 15:43:57 +0100

    Add transitions

--- public/src/web_modules/Hello.vue
+++ public/src/web_modules/Hello.vue
@@ -1,19 +1,57 @@
 <template>
-  <div>
+  <div transition="expand">
     <h2 class="title">Hello</h2>
     <a v-link="'/'">Back</a>
   </div>
 </template>
 
 <script>
+
+  import Vue from 'vue';
+  Vue.transition('expand', {
+    beforeEnter: () => {
+      console.log('before enter');
+    },
+    enter: () => {
+      console.log('enter');
+    },
+    afterEnter: () => {
+      console.log('after enter');
+    },
+    beforeLeave: () => {
+      console.log('before leave');
+    },
+    leave: () => {
+      console.log('leave');
+    },
+    afterLeave: () => {
+      console.log('after leave');
+    }
+  });
+
   export default {
     // ...
   }
+
 </script>
 
 <style>
   .title {
     font-size: 19px;
     color: green;
+  }
+
+  .expand-transition {
+    transition: all .3s ease;
+    height: 100px;
+    padding: 10px;
+    background-color: #eee;
+    overflow: hidden;
+  }
+
+  .expand-enter, .expand-leave {
+    height: 0;
+    padding: 0 10px;
+    opacity: 0;
   }
 </style>
