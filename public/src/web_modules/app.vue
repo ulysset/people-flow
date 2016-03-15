2b522e8514c349bafd480ce744b9053e24aa5e1e
Luca <haenell@yahoo.fr>
2016-03-15 19:58:48 +0100

    fonts & invent

--- public/src/web_modules/app.vue
+++ public/src/web_modules/app.vue
@@ -11,6 +11,11 @@
     src: url('../assets/fonts/Brandon-Regular.otf');
   }
 
+    @font-face {
+      font-family: 'Brandon_thin';
+      src: url('../assets/fonts/Brandon_thin.otf');
+    }
+
   body {
     margin: auto;
     font-family: "Brandon";
--- public/src/web_modules/event.vue
+++ public/src/web_modules/event.vue
@@ -3,14 +3,23 @@
       <img src="../assets/img/Logo.svg"/ class="logo">
       <li><a v-link="'/sources'" class="link">Sources</a></li>
       <li><a v-link="'/'" class="link">Copyright</a></li>
-      <li><a v-link="'https://github.com/ulysset/people-flow'" class="link">Github</a></li>
-      <li><a v-link="'/'" class="link">Partager</a></li>
+      <li><a href="'https://github.com/ulysset/people-flow'" class="link">Github</a></li>
+      <li><a href="https://twitter.com/intent/tweet?text=Futuregrant&url=http://localhost:8080/#!/event" target="_blank" class="link">Partager</a></li>
       <li class="help"><a v-link="'/'" class="link">BESOIN D'AIDE ?</a></li>
     </ul>
 
   <div class="container">
-    <h2 class="title">Events</h2>
-    <a class="back" v-link="'/map'">Back</a>
+
+    <a v-link="'/map'"><img class="back" src="../assets/img/close.svg"/></a>
+
+    <div class="events">
+    <img src="../assets/img/left_arrow.svg"/>
+    <p>ÉVÈNEMENTS 2014</p>
+    <img src="../assets/img/right_arrow.svg"/>
+    </div>
+
+    <h1>Guerre en Syrie</h1>
+
   </div>
 </template>
 
@@ -60,6 +69,13 @@
     padding-top:100px;
     margin: auto;
     text-align: center;
+
+    h1{
+      font-size:38px;
+      font-family: 'Brandon_thin';
+      color:#a8a8a8;
+      padding-top:40px;
+    }
   }
 
   .title {
@@ -69,11 +85,43 @@
   }
 
   .back {
-    color: grey;
+    width:20px;
+    height:20px;
+    padding:20px;
+    background:#D6D6D6;
+    border-radius:50%;
 
     &:hover {
-      color: black;
+      opacity:.7;
     }
+  }
+
+
+  .events{
+    padding-top:50px;
+
+    img {
+      width:25px;
+      height:25px;
+      padding:10px;
+      background:#e6e6e6;
+      border-radius:50%;
+      display:inline-block;
+      cursor:pointer;
+
+      &:hover{
+        opacity:.7;
+      }
+    }
+
+    p {
+      margin:0;
+      display:inline-block;
+      font-size:20px;
+      padding:0 30px;
+      color:#a8a8a8;
+    }
+
   }
 
 </style>
@@ -90,8 +138,3 @@
   // }
 
 </script>
-
-<style lang="sass" scoped>
-
-
-</style>
