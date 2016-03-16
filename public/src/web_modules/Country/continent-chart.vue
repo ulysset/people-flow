e27fd1186a0435ec7442bf786860def785ba763f
ULYSSE TALLEPIED <ulysse.tallepied@hetic.net>
2016-03-16 22:42:34 +0100

    continennt leo

--- public/src/web_modules/Country/continent-chart.vue
+++ public/src/web_modules/Country/continent-chart.vue
@@ -1,78 +1,85 @@
 <template>
-  <div v-for="continent in continents" v-on:mouseOver="onHover(this, continent.name)">
-    <span v-bind:class="{
-      'continentName': true,
-      'enabled': activeContinent == continent.name
-    }">{{ continent.name }}</span>
-    <div
-      v-for="continentItem in continent.count"
-      class="item"
-      v-bind:class="continent.name">
-    </div>
-  </div>
+    <div v-for="continent in continents" v-on:mouseOver="onHover(this, continent.name)" >
+        <span v-bind:class="{'enabled': activeContinent == continent.name}">{{ continent.name }}</span>
+        <div v-for="continentItem in continent.count" class="item" v-bind:class="continent.name">
+        </div>
+    </div>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        activeContinent: '',
-        continents: [{
-          name: 'africa',
-          count: new Array(4)
-        }, {
-          name: 'america',
-          count: new Array(1)
-        }]
-      }
-    },
+export default {
+    data() {
+            return {
+                activeContinent: '',
+                continents: [{
+                    name: 'africa',
+                    migrants  : 2345,
+                    count : new Array()
+                }, {
+                    name: 'america',
+                    migrants : 4899,
+                    count : new Array()
+                }]
+            }
+    },
+    ready() {
+        const total = this.continents
+            .map(item => item.migrants)
+            .reduce((a, b) => a + b)
 
-    methods: {
-      onHover: (ctx, name) => {
-        ctx.activeContinent = name;
-      }
-    }
-  }
+        this.continents.forEach((item) => {
+            let proportion = Math.ceil((item.migrants / total) * 20)
+            item.count = new Array(proportion)
+            console.log('**')
+            console.log(proportion)
+            console.log(item.count)
+        })
 
+    },
 
+    methods: {
+        onHover: (ctx, name) => {
+            ctx.activeContinent = name;
+        }
+    }
+}
 </script>
 
 <style scoped>
+    .item {
+        width: 10px;
+        height: 60px;
+        display: inline-block;
+        border-radius: 10px;
+        margin: 0 3px;
+    }
 
-.item {
-    width: 10px;
-    height: 60px;
-    display: inline-block;
-    border-radius: 10px;
-    margin: 0 3px;
-}
+    .africa {
+        background-color: red;
+    }
 
-.africa {
-    background-color: red;
-}
+    .asia {
+        background-color: blue;
+    }
 
-.asia {
-    background-color: blue;
-}
+    .america {
+        background-color: coral;
+    }
 
-.america {
-    background-color: coral;
-}
+    .oceania {
+        background-color: purple;
+    }
 
-.oceania {
-    background-color: purple;
-}
+    .europe {
+        background-color: green;
+    }
 
-.europe {
-    background-color: green;
-}
+    .continentName {
+        opacity: 0;
+    }
 
-.continentName {
-    opacity: 0;
-}
-
-.enabled {
-    opacity: 1;
-}
+    .enabled {
+        opacity: 1;
+    }
 
 </style>
