756170963ff68cf9ada0e3756144dc751ba59d16
ULYSSE TALLEPIED <ulysse.tallepied@hetic.net>
2016-03-16 13:17:05 +0100

    :poop:

--- public/src/web_modules/Country/continent-chart.vue
+++ public/src/web_modules/Country/continent-chart.vue
@@ -1,89 +1,76 @@
 <template>
-    <canvas v-el:chart class="chart-continent"></canvas>
-    <div v-for="continent in continents" class="item" v-bind:class="continent.name"></div>
+  <div v-for="continent in continents" v-on:mouseOver="onHover(this, continent.name)">
+    <span v-bind:class="{
+      'continentName': true,
+      'enabled': activeContinent == continent.name
+    }">{{ continent.name }}</span>
+    <div
+      v-for="continentItem in continent.count"
+      class="item"
+      v-bind:class="continent.name">
+    </div>
+  </div>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        activeContinent: '',
+        continents: [{
+          name: 'africa',
+          count: new Array(4)
+        }, {
+          name: 'america',
+          count: new Array(1)
+        }]
+      }
+    },
 
-    export default {
-        data() {
-            return {
-                continents: [],
-                data: [{
-                    name: 'Africa',
-                    migrants: 36676,
-                }, {
-                    name: 'Asia',
-                    migrants: 3674,
-                }, {
-                    name: 'Oceania',
-                    migrants: 2556,
-                }, {
-                    name: 'America',
-                    migrants: 23,
-                }, {
-                    name: 'Europe',
-                    migrants: 255,
-                }]
-            }
-        },
-
-        ready() {
-            const total = this.data
-                .map(item => item.migrants)
-                .reduce((a, b) => a + b);
-
-            this.data.forEach((item) => {
-                let proportion = (item.migrants /total)*20
-                if (proportion < 0.2) {
-
-                }
-                console.log(proportion);
-                this.continents.push({name : item.name.toLowerCase()})
-            })
-
-
-
-
-
-
-        }
-    }
+    methods: {
+      onHover: (ctx, name) => {
+        ctx.activeContinent = name;
+      }
+    }
+  }
+>>>>>>> Stashed changes
 
 </script>
 
-
 <style scoped>
-    .chart-continent {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-    }
 
-    .item {
-        width: 10px;
-        height: 60px;
-        display: inline-block;
-        border-radius: 10px;
-        margin: 0 3px;
-    }
+  .item {
+    width: 10px;
+    height: 60px;
+    display: inline-block;
+    border-radius: 10px;
+    margin: 0 3px;
+  }
 
-    .africa {
-        background-color: red;
-    }
+  .africa {
+    background-color: red;
+  }
 
-    .asia {
-        background-color: blue;
-    }
-    .america {
-        background-color: coral;
-    }
+  .asia {
+    background-color: blue;
+  }
+  .america {
+    background-color: coral;
+  }
 
-    .oceania {
-        background-color: purple;
-    }
-    .europe {
-        background-color: green;
-    }
+  .oceania {
+    background-color: purple;
+  }
+  .europe {
+    background-color: green;
+  }
+
+  .continentName {
+    opacity: 0;
+  }
+
+  .enabled {
+    opacity: 1;
+  }
+
 </style>
