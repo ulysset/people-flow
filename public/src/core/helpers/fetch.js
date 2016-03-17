1ab83a1f1d6d5cba619dbf6bfcfea2e6aefb27da
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-03-17 15:26:14 +0100

    [Util] Fix fetch

--- public/src/core/helpers/fetch.js
+++ public/src/core/helpers/fetch.js
@@ -7,20 +7,18 @@
  * @param {object} headers
  * @return {object}
  */
-const fetch = (url, { method = 'GET', body = null, headers = {} } = {} ) => {
-    const xhr = new XMLHttpRequest();
-    return new Promise(resolve => {
-        xhr.open(method, url, true);
-        Object.keys(headers).forEach((key) => {
-            xhr.setRequestHeader(key.toLowerCase(), headers[key]);
-        });
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState === 4 && xhr.status !== 0 && xhr.statusText !== 'abort') {
-                resolve(JSON.parse(xhr.response));
-            }
-        };
-        xhr.send(body);
+export default (url, { method = 'GET', body = null, headers = {} } = {} ) => {
+  const xhr = new XMLHttpRequest();
+  return new Promise(resolve => {
+    xhr.open(method, url, true);
+    Object.keys(headers).forEach((key) => {
+      xhr.setRequestHeader(key.toLowerCase(), headers[key]);
     });
+    xhr.onreadystatechange = () => {
+      if(xhr.readyState === 4 && xhr.status !== 0 && xhr.statusText !== 'abort') {
+        resolve(xhr.response);
+      }
+    };
+    xhr.send(body);
+  });
 };
-
-export default fetch;
