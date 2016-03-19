d46ec6c3ea6b619012903db67429693bc8e16273
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-03-19 14:22:50 +0100

    [Map] Fix alpha color

--- public/src/core/helpers/scene.js
+++ public/src/core/helpers/scene.js
@@ -7,6 +7,7 @@ export const createScene = (selector: string): any => {
   const renderer = new PIXI.WebGLRenderer(window.innerWidth, window.innerHeight, {
     retina: 2,
     transparent: true,
+    backgroundColor: '#b8c1ca', // Need for alpha color
     antialias: true,
     view: selector
   });
