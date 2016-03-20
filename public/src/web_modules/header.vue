cde8693bf1735b977a67a0587f452f137af71841
yohan-atlan <yohan.atlan@hetic.net>
2016-03-20 22:44:41 +0100

    Add facebook and Twitter share :poop:

--- public/src/web_modules/header.vue
+++ public/src/web_modules/header.vue
@@ -4,8 +4,8 @@
     <nav class="nav">
       <ul class="nav-list">
         <li class="nav-item --help"><a href="https://github.com/ulysset/people-flow" class="nav-link --share --github"></a></li>
-        <li class="nav-item --help"><a href="#" target="_blank" class="nav-link --share --facebook"></a></li>
-        <li class="nav-item --help"><a href="https://twitter.com/intent/tweet?text=Futuregrant&url=https://still-fortress-88655.herokuapp.com" target="_blank" class="nav-link --share --twitter"></a></li>
+        <li class="nav-item --help"><a href="https://www.facebook.com/sharer/sharer.php?u=https://still-fortress-88655.herokuapp.com" target="_blank" class="nav-link --share --facebook"></a></li>
+        <li class="nav-item --help"><a href="https://twitter.com/intent/tweet?text=Futuregrant la nouvelle dataviz qui change notre regard !&url=https://still-fortress-88655.herokuapp.com" target="_blank" class="nav-link --share --twitter"></a></li>
       </ul>
     </nav>
   </header>
