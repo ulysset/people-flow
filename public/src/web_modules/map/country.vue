f0b78633e41ed8666b14d4421291714538834b4e
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-03-20 10:53:58 +0100

    [Map] Add country component

---
+++ public/src/web_modules/map/country.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="country">
+    <span class="title">France</span>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+
+  @import "../theme";
+
+  .country {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    left: 50px;
+    z-index: 5;
+  }
+
+  .title {
+    color: $secondaryColor;
+    font-size: 22px;
+  }
+
+</style>
