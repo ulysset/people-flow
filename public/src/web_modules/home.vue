8a35827873961588a34e0d84c79ede8f303443bf
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-03-21 12:43:52 +0100

    [Home] New ui

--- public/src/web_modules/home.vue
+++ public/src/web_modules/home.vue
@@ -5,8 +5,11 @@
         src="https://youtube.com/embed/tw-q-hG0owc?loop=1&autoplay=1&controls=0&showinfo=0&autohide=1&playlist=tw-q-hG0owc">
       </iframe>
     </div>
-    <a v-link="'/'"><img src="../assets/img/logo.svg"/ class="logo"></a>
-    <a class="button" v-link="'/map'">Découvrir l'expérience</a>
+    <div class="container_home">
+      <a v-link="'/'"><img src="../assets/img/logo.png"/ class="logo"></a>
+      <p class="description">Une carte interactive humaine</p>
+      <a class="button" v-link="'map'">COMMENCER</a>
+    </div>
   </div>
 </template>
 
@@ -14,28 +17,42 @@
 
   @import "./theme";
 
+  .container_home {
+    width:600px;
+    text-align: center;
+    z-index:10;
+    position:absolute;
+    left:calc(50% - 300px);
+    top:50%;
+    transform:translateY(-50%);
+  }
+
+  .description {
+    font-size:20px;
+    margin: 2px 0 10px;
+    color:white;
+    text-transform:uppercase;
+    font-family:'Brandon_thin';
+  }
+
   .logo{
-    width:150px;
-    margin: 24px 30px;
-    opacity: .25;
+    width:300px;
+    margin: 20px 0 20px;
   }
 
   .button {
-    display: block;
-    position: absolute;
-    left: 50%;
-    bottom: 75px;
-    transform: translateX(-50%);
-    padding: 15px 22px 16px;
-    background-color: white;
-    border-radius: 3px;
-    text-transform: uppercase;
+    width:150px;
+    display: inline-block;
+    padding: 17px;
+    border-radius: 50px;
     text-decoration: none;
-    color: $primaryColor;
-    font-size: 14px;
+    margin-top:40px;
+    border: 3px solid white;
+    color:white;
+    transition:all ease .6s;
 
-    &:hover {
-      color: rgba($primaryColor, .5);
+    &:hover{
+      opacity: .75;
     }
   }
 
